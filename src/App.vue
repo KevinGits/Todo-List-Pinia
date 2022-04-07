31fe8719cdc7b9153c96dbf15966e0b86f09c356
KevinGits <kevin@digits.id>
2022-04-07 13:28:48 +0700

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <router-link to="/">Home</router-link>
+                </li>
+                <li>
+                    <router-link to="/Profile">Profile</router-link>
+                </li>
+                <li>
+                    <router-link to="/Login">Login</router-link>
+                </li>
+                <li>
+                    <router-link to="/TodosView">Todo</router-link>
+                </li>
+            </ul>
+
+        </div>
+        <router-view />
+    </div>
+
+</template>
+
+<script>
+    import 'https://use.fontawesome.com/releases/v6.1.0/js/all.js'
+</script>
+
+<style>
+    @import url('https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css');
+    @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500');
+    @import './assets/base.css';
+</style>
---
+++ src/pages/Profile/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>This is an Profile Page</h1>
+    <h2>Hello, my name is Kevin Hindra Jaya</h2>
+    <h3>This is my project page about to do - List</h3>
+</template>
+
+<style>
+</style>
