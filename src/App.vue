604be7db31590a6586e2a93264f44c42a130ca35
KevinGits <kevin@digits.id>
2022-04-07 15:14:09 +0700

    Update

--- src/App.vue
+++ src/App.vue
@@ -25,12 +25,13 @@
 
 </template>
 
-<script>
+<script lang="ts">
     import 'https://use.fontawesome.com/releases/v6.1.0/js/all.js'
+    export default {};
 </script>
 
 <style>
     @import url('https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css');
     @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500');
-    @import './assets/base.css';
+    @import './assets/base.css';    
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
+import Application from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const app = createApp(Application)
 
 app.use(createPinia())
 app.use(router)
